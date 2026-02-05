7539ca25b8af58cc8206eaf9cd323ef094d2f34f
Poovarasan S <poovarasan936161@gmail.com>
2026-02-05 15:03:07 +0530

    fix:list view

--- src/Pages/Events/EventsPage.jsx
+++ src/Pages/Events/EventsPage.jsx
@@ -346,7 +346,7 @@ export default function EventsPage() {
       >
         <Nav.Item>
           <Nav.Link eventKey="list" className="px-4">
-            View List
+            View Events
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
