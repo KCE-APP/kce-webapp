aae8a97c0a1f7a36c84dbee4cec4329cb3a26047
Poovarasan S <poovarasan936161@gmail.com>
2026-02-14 01:02:51 +0530

    fix:sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -22,126 +22,127 @@ function Sidebar() {
         <Nav.Link
           as={NavLink}
           to="/achievers"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.achievers}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Achievers</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/events"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.events}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Events</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/users"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.users}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Users</span>
         </Nav.Link>
-        {/* <Nav.Link as={NavLink} to="/announcements" className="sidebar-link d-flex align-items-center gap-2 px-3 py-2 rounded text-decoration-none">
+        {/* <Nav.Link as={NavLink} to="/announcements" className="sidebar-link d-flex align-items-center gap-2 px-1 py-2 rounded text-decoration-none">
                  <span className="fw-medium">Announcements</span>
             </Nav.Link> */}
         <Nav.Link
           as={NavLink}
           to="/api-integration"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.arena}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Arena</span>
+        </Nav.Link>
+                <Nav.Link
+          as={NavLink}
+          to="/achieve-management"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
+        >
+          <img
+            src={icons.verfication}
+            alt="icon"
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
+          />
+          <span className="fw-medium">Submission Approval</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/point-rules"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.rules}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Point Rules</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/staff"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.staff}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Manage Staff</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/semester"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.semester}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Manage Semester</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/reward-catalog"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.catalog}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Reward Catalog</span>
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/redemption-history"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
+          className="sidebar-link d-flex align-items-center gap-3 px-1 py-2 rounded text-decoration-none"
         >
           <img
             src={icons.history}
             alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
+            style={{ width: "20px", height: "20px", objectFit: "contain" }}
           />
           <span className="fw-medium">Redemption History</span>
         </Nav.Link>
-        <Nav.Link
-          as={NavLink}
-          to="/achieve-management"
-          className="sidebar-link d-flex align-items-center gap-3 px-3 py-2 rounded text-decoration-none"
-        >
-          <img
-            src={icons.verfication}
-            alt="icon"
-            style={{ width: "24px", height: "24px", objectFit: "contain" }}
-          />
-          <span className="fw-medium">Submission Approval</span>
-        </Nav.Link>
+
       </Nav>
     </div>
   );
